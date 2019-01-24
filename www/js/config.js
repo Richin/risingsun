07e83870880b6a32a274309c19f0177597f1c044
Richin Varghese <richin1990@gmail.com>
2019-01-24 14:11:04 +0530

    updated

---
+++ www/js/config.js
@@ -0,0 +1,9 @@
+/*Driver App Configuration*/
+
+var krms_driver_config ={
+	'ApiUrl':"http://risingsun.akstech.com.sg/driver/api",
+	'DialogDefaultTitle':"DriverApp",
+	'mapboxToken' : '',
+	'APIHasKey':"mykey",
+	'debug': false
+};
